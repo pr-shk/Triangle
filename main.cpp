5bd942647f45fb8328bf349323bbf4f073eff602
Paulo Ventura <pr.shk+git@msn.com>
2025-03-31 11:11:19 -0300

    hueing colors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <windows.h>
 
+// Global variables to control the triangle color
+COLORREF triangleColor = RGB(0, 0, 0);  // Starts Black
+int r = 0, g = 0, b = 0;  // RGB values
+
 // Window callback function - handles all window messages
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -43,6 +47,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
+    // Set up a timer to change the triangle color every 500ms
+    SetTimer(hwnd, 0.1, 33, nullptr);
+
     // Message loop - handles all Windows messages
     MSG msg = {};
     while (GetMessage(&msg, nullptr, 0, 0)) {
@@ -61,14 +68,80 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             PostQuitMessage(0);    // Quit the application
             return 0;
 
+        case WM_TIMER:
+        {
+            // Update the triangle color
+            static bool increasing = true;
+            static int currentColor = 0;  // 0 = blue, 1 = green, 2 = red
+            
+            if (currentColor == 0) {
+                // Handle blue color
+                if (increasing) {
+                    b++;
+                    if (b >= 255) {
+                        b = 255;
+                        currentColor = 1;
+                        increasing = true;
+                    }
+                } else {
+                    b--;
+                    if (b <= 0) {
+                        b = 0;
+                        currentColor = 1;
+                        increasing = false;
+                    }
+                }
+            }
+            else if (currentColor == 1) {
+                // Handle green color
+                if (increasing) {
+                    g++;
+                    if (g >= 255) {
+                        g = 255;
+                        currentColor = 2;
+                        increasing = true;
+                    }
+                } else {
+                    g--;
+                    if (g <= 0) {
+                        g = 0;
+                        currentColor = 2;
+                        increasing = false;
+                    }
+                }
+            }
+            else if (currentColor == 2) {
+                // Handle red color
+                if (increasing) {
+                    r++;
+                    if (r >= 255) {
+                        r = 255;
+                        currentColor = 0;
+                        increasing = false;
+                    }
+                } else {
+                    r--;
+                    if (r <= 0) {
+                        r = 0;
+                        currentColor = 0;
+                        increasing = true;
+                    }
+                }
+            }
+            
+            triangleColor = RGB(r, g, b);
+            InvalidateRect(hwnd, nullptr, TRUE);
+            return 0;
+        }
+
         case WM_PAINT:
         {
             // Begin painting
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
             
-            // Create and select a red brush
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
+            // Create and select a brush with the current color
+            HBRUSH hBrush = CreateSolidBrush(triangleColor);
             SelectObject(hdc, hBrush);
 
             // Define triangle vertices
@@ -80,6 +153,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
             // Draw the triangle
             Polygon(hdc, points, 3);
+
+            // Set text color to black for better visibility
+            SetTextColor(hdc, RGB(0, 0, 0));
+            
+            // Create text buffer for RGB values
+            wchar_t text[50];
+            wsprintfW(text, L"R: %d\n G: %d\n B: %d", r, g, b);
+            
+            // Draw the text
+            TextOutW(hdc, 20, 20, text, wcslen(text));
 
             // Clean up resources
             DeleteObject(hBrush);
